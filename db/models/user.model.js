ae57f97448563be0034cb564bfe7a057ef223dd5
mscalel <msksaul@gmail.com>
2022-10-20 20:56:59 -0600

    ORM Migrations

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -23,6 +23,11 @@ const UserSchema = {
     type: DataTypes.DATE,
     field: 'created_at',
     defaultValue: Sequelize.NOW
+  },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'customer'
   }
 }
 
