0fbf20e4fbb3c74357916387ce3d1fcf5f932fc5
mscalel <msksaul@gmail.com>
2022-10-22 16:00:32 -0600

    Filtering and Pagination

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -6,6 +6,11 @@ const name = Joi.string().min(3).max(15)
 const price = Joi.number().integer().min(10)
 const description = Joi.string().min(10)
 const image = Joi.string().uri();
+const limit = Joi.number().integer()
+const offset = Joi.number().integer()
+
+const price_min = Joi.number().integer()
+const price_max = Joi.number().integer()
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -13,7 +18,7 @@ const createProductSchema = Joi.object({
   description: description.required(),
   image: image.required(),
   categoryId: categoryId.required()
-});
+})
 
 const updateProductSchema = Joi.object({
   name: name,
@@ -21,10 +26,21 @@ const updateProductSchema = Joi.object({
   image: image,
   description,
   categoryId
-});
+})
 
 const getProductSchema = Joi.object({
   id: id.required(),
-});
+})
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema }
+const queryProductSchema = Joi.object({
+  limit,
+  offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer(),
+    then: Joi.required()
+  })
+})
+
+module.exports = { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema }
