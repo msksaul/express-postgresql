413439963a7df414c1f3d4165e4179dc8f01fe8d
mscalel <msksaul@gmail.com>
2022-10-24 15:34:42 -0600

    Passport Login

---
+++ routes/auth.router.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router();
+
+const passport = require('passport')
+
+router.post('/login', passport.authenticate('local', { session: false }),
+  async (req, res, next) => {
+    try {
+      res.json(req.user)
+    } catch (error) {
+      next(error);
+    }
+  }
+)
+
+module.exports = router;
